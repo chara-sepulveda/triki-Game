8736c11478ca7637efe20bf9c19c9501e18735fa
Daniel Valencia <charaprogram19@gmail.com>
2022-08-08 09:34:22 -0500

    inicializamos variables

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+gitlet celdad = ['','','','','','','','','']
+let jugadorActual='x'
+let resultado =document.querySelector('.result')
+let botone=document.querySelectorAll('.btn')
+
+let condiciones = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [6,4,8],
+]
