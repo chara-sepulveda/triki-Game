5f446d21fe2ed13ae2f2b9e00f7f48e93b3efc0b
Daniel Valencia <charaprogram19@gmail.com>
2022-08-08 10:26:03 -0500

    creamos arreglos y funciones

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
-gitlet celdad = ['','','','','','','','','']
+let celdas = ['','','','','','','','','']
 let jugadorActual='x'
-let resultado =document.querySelector('.result')
-let botone=document.querySelectorAll('.btn')
+let resultado =document.querySelector('.result');
+let botones=document.querySelectorAll('.btn');
 
 let condiciones = [
     [0,1,2],
@@ -13,3 +13,16 @@ let condiciones = [
     [2,4,6],
     [6,4,8],
 ]
+//desde aca empieza la logica
+ const triki =(element,i)=>{
+element.value=jugadorActual;
+element.disable = true;
+celdas[i]=jugadorActual
+jugadorActual= jugadorActual=='x'? 'o' : 'x';
+ 
+ resultado.innerHTML=`player ${jugadorActual}turn`
+}
+botones.forEach((boton,indice)=>{
+    boton.addEventListener('click',()=>{triki(boton,indice)})
+})
+
